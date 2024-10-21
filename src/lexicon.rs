01eb5920fdc6bb5bf2ed237be79af01ca0badadc
dataphract <dataphract@gmail.com>
2024-10-21 14:06:29 -0500

    initial commit

---
+++ src/lexicon.rs
@@ -0,0 +1,274 @@
+use std::collections::BTreeMap;
+
+use serde::{de::Unexpected, Deserialize};
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Lexicon {
+    /// The Lexicon language version used by this definition.
+    pub lexicon: i64,
+    /// The NSID of this Lexicon.
+    pub id: std::string::String,
+    /// The version of this Lexicon, if changes have occurred.
+    pub revision: Option<i64>,
+    pub description: Option<std::string::String>,
+    pub defs: BTreeMap<std::string::String, Schema>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(tag = "type", rename_all = "kebab-case")]
+pub enum Schema {
+    Array(Array),
+    Blob(Blob),
+    Boolean(Boolean),
+    Bytes(Bytes),
+    CidLink,
+    Integer(Integer),
+    Null,
+    Object(Object),
+    Params(Params),
+    Procedure(Procedure),
+    Query(Query),
+    Record(Record),
+    Ref(Ref),
+    String(String),
+    Subscription(Subscription),
+    Token,
+    Union(Union),
+    Unknown,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Array {
+    pub description: Option<std::string::String>,
+    pub items: Box<Schema>,
+    pub min_length: Option<i64>,
+    pub max_length: Option<i64>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Blob {
+    pub description: Option<std::string::String>,
+    pub accept: Option<Vec<std::string::String>>,
+    pub max_size: Option<i64>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Boolean {
+    pub description: Option<std::string::String>,
+    pub default: Option<bool>,
+    #[serde(rename = "const")]
+    #[doc(alias = "const")]
+    pub is_const: Option<bool>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Bytes {
+    pub description: Option<std::string::String>,
+    pub min_length: Option<i64>,
+    pub max_length: Option<i64>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Integer {
+    pub description: Option<std::string::String>,
+    pub minimum: Option<i64>,
+    pub maximum: Option<i64>,
+    #[serde(rename = "enum")]
+    #[doc(alias = "enum")]
+    pub enum_values: Option<Vec<i64>>,
+    pub default: Option<i64>,
+    #[serde(rename = "const")]
+    #[doc(alias = "const")]
+    pub is_const: Option<bool>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Object {
+    pub description: Option<std::string::String>,
+    pub properties: BTreeMap<std::string::String, Schema>,
+    #[serde(default)]
+    pub required: Vec<std::string::String>,
+    #[serde(default)]
+    pub nullable: Vec<std::string::String>,
+}
+
+/// The input of a procedure or query.
+#[derive(Clone, Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Input {
+    pub description: Option<std::string::String>,
+    pub encoding: std::string::String,
+    pub schema: Option<IoSchema>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(tag = "type", rename_all = "kebab-case")]
+pub enum IoSchema {
+    Object(Object),
+    Ref(Ref),
+    Union(Union),
+}
+
+/// The output of a procedure or query.
+#[derive(Clone, Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Output {
+    pub description: Option<std::string::String>,
+    pub encoding: std::string::String,
+    pub schema: Option<IoSchema>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Params {
+    pub description: Option<std::string::String>,
+    pub required: Option<Vec<std::string::String>>,
+    pub properties: BTreeMap<std::string::String, Schema>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Error {
+    pub name: std::string::String,
+    pub description: Option<std::string::String>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Procedure {
+    pub description: Option<std::string::String>,
+    pub parameters: Option<Box<Schema>>,
+    pub output: Option<Output>,
+    pub input: Option<Input>,
+    pub errors: Option<Vec<Error>>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Query {
+    pub description: Option<std::string::String>,
+    pub parameters: Option<Box<Schema>>,
+    pub output: Option<Output>,
+    pub errors: Option<Vec<Error>>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Record {
+    pub description: Option<std::string::String>,
+    pub key: std::string::String,
+    #[serde(deserialize_with = "Record::deserialize_record_field")]
+    pub record: Object,
+}
+
+impl Record {
+    fn deserialize_record_field<'de, D>(des: D) -> Result<Object, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        use serde::de::Error;
+
+        let schema = Schema::deserialize(des)?;
+        match schema {
+            Schema::Object(object) => Ok(object),
+            _ => Err(D::Error::invalid_value(
+                Unexpected::Enum,
+                &"a schema of type `object`",
+            )),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Ref {
+    pub description: Option<std::string::String>,
+    #[serde(rename = "ref")]
+    #[doc(alias = "ref")]
+    pub ref_: std::string::String,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+pub struct String {
+    pub description: Option<std::string::String>,
+    pub format: Option<std::string::String>,
+    pub max_length: Option<i64>,
+    pub min_length: Option<i64>,
+    pub max_graphemes: Option<i64>,
+    pub min_graphemes: Option<i64>,
+    #[serde(default)]
+    pub known_values: Vec<std::string::String>,
+    #[serde(rename = "enum")]
+    #[doc(alias = "enum")]
+    pub enum_values: Option<Vec<std::string::String>>,
+    pub default: Option<std::string::String>,
+    #[serde(rename = "const")]
+    #[doc(alias = "const")]
+    pub const_value: Option<std::string::String>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(deny_unknown_fields, rename_all = "kebab-case")]
+pub enum StringFormat {
+    AtIdentifier,
+    AtUri,
+    Cid,
+    Datetime,
+    Did,
+    Handle,
+    Nsid,
+    Tid,
+    RecordKey,
+    Uri,
+    Language,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Subscription {
+    pub description: Option<std::string::String>,
+    pub parameters: Option<Box<Schema>>,
+    pub message: Option<Message>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Message {
+    pub description: Option<std::string::String>,
+    pub schema: Box<Schema>,
+    pub errors: Option<Vec<Error>>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct Union {
+    pub description: Option<std::string::String>,
+    pub refs: Vec<std::string::String>,
+    #[serde(default)]
+    pub closed: bool,
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use super::*;
+
+    #[test]
+    fn lexicon_empty_defs() {
+        serde_json::from_value::<Lexicon>(json!({
+            "lexicon": 1,
+            "id": "com.example.whatever",
+            "defs": {},
+        }))
+        .expect("should parse successfully");
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod lexicon;
+
+pub use lexicon::*;
